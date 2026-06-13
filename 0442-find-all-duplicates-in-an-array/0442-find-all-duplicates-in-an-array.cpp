92c66601365aa3cf5f2e8dfc4a7f085da67b51b0
chaudharyayush7409-afk <chaudharyayush7409@gmail.com>
2026-06-14 02:01:17 +0530

    Time: 67 ms (7.91%), Space: 67.1 MB (15.76%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        unordered_map<int,int>freq;
+
+        for(int i=0; i<nums.size(); i++){
+            freq[nums[i]]++;
+        }
+
+        vector<int>ans;
+        for(auto it:freq){
+            if(it.second >= 2){
+                ans.push_back(it.first);
+            }
+        }
+        return ans;
+    }
+};
