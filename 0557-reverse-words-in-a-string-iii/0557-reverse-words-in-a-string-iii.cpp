2726b868b0b609bb1a7824bc656a51172e99d50b
chaudharyayush7409-afk <chaudharyayush7409@gmail.com>
2026-06-20 21:10:12 +0530

    Time: 0 ms (100%), Space: 12.2 MB (72.55%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -2,31 +2,49 @@ class Solution {
 public:
     string reverseWords(string s) {
         
-        string result = "";
-        string words = "";
-        for(int i=0; i<s.length(); i++){
+        // string result = "";
+        // string words = "";
+        // for(int i=0; i<s.length(); i++){
             
-            if(s[i]==' '){
-                string rev = "";
-                for(int i=words.length()-1; i>=0; i--){
-                    rev+=words[i];
-                }
+        //     if(s[i]==' '){
+        //         string rev = "";
+        //         for(int i=words.length()-1; i>=0; i--){
+        //             rev+=words[i];
+        //         }
 
-                result+=rev+" ";
-                words = "";
+        //         result+=rev+" ";
+        //         words = "";
 
-            }
-            else{
-                words+=s[i];
-            }
-        }
+        //     }
+        //     else{
+        //         words+=s[i];
+        //     }
+        // }
         
-        string rev = "";
-        for(int j=words.length()-1; j>=0; j--){
-            rev+=words[j];
+        // string rev = "";
+        // for(int j=words.length()-1; j>=0; j--){
+        //     rev+=words[j];
+        // }
+        // result+=rev;
+        // return result;
+
+
+        int n = s.length();
+
+        int start = 0;
+
+        for(int i=0; i<=n; i++){
+            if(i==n || s[i]==' '){
+                reverse(s.begin()+start, s.begin()+i);
+                start = i+1;
+            }
         }
-        result+=rev;
-        return result;
+        return s;
+
+
+
+
+
     }
    
 };
